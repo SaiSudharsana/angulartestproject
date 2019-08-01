8cc23a9f8b3f42507d8d136bb2f60bb283da983d
Sai Sudharsana Veeraghavan <sai.veeraghavan@corp.ads.valuelabs.net>
2019-08-01 15:47:15 +0530

    READ operation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListemployeesComponent } from './employees/listemployees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListemployeesComponent
   ],
   imports: [
     BrowserModule,
