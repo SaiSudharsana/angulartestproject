d6af980883e09b406a36393e9afbfd95a90c1654
Sai Sudharsana Veeraghavan <sai.veeraghavan@corp.ads.valuelabs.net>
2019-08-01 16:18:05 +0530

    routing performed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListemployeesComponent } from './employees/listemployees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
 
+
+const appRoutes: Routes = [
+  { path: 'list', component: ListemployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    ListemployeesComponent
+    ListemployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
