05c6884bef5e880c3da4fa07b0a297f2c6e54d7a
Sai Sudharsana Veeraghavan <sai.veeraghavan@corp.ads.valuelabs.net>
2019-08-06 12:16:09 +0530

    route params used to reroute to new page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,23 @@ import { CreateEmployeeComponent } from './employees/create-employee.component';
 import { EmployeeService } from './employees/employee.service';
 import { DisplayEmployeeComponent } from './employees/display-employee.component';
 import { CreateEmployeeCanDeactivateGuardService } from './employees/create-employee-can-deactivate-gaurd.service';
-
+import { EmployeeDetailsComponent } from './employees/employee-details.component';
 const appRoutes: Routes = [
   { path: 'list', component: ListemployeesComponent },
   {
     path: 'create',   component: CreateEmployeeComponent,
     canDeactivate: [CreateEmployeeCanDeactivateGuardService]
   },
+  { path: 'employees/:id', component: EmployeeDetailsComponent },
   { path: '', redirectTo: '/list', pathMatch: 'full' }
-];
+   ];
 @NgModule({
   declarations: [
     AppComponent,
     ListemployeesComponent,
     CreateEmployeeComponent,
-    DisplayEmployeeComponent
+    DisplayEmployeeComponent,
+    EmployeeDetailsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/employees/listemployees.component.ts
+++ src/app/employees/listemployees.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee.model';
 import { EmployeeService } from './employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-employees',
@@ -10,24 +11,26 @@ import { EmployeeService } from './employee.service';
 export class ListemployeesComponent implements OnInit {
 
   employees: Employee[];
-  employeeToDisplay: Employee;
-  private arrayIndex=1;
-  constructor(private _employeeService: EmployeeService) { }
+  //employeeToDisplay: Employee;
+  //private arrayIndex=1;
+  constructor(private _employeeService: EmployeeService, private _router: Router) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
-    this.employeeToDisplay = this.employees[0];
+    //this.employeeToDisplay = this.employees[0];
   }
-  nextEmployee(): void{
+  /*nextEmployee(): void{
          if(this.arrayIndex<=2)
           {
           this.employeeToDisplay=this.employees[this.arrayIndex];
           this.arrayIndex++;
           }
-  else  {
-    this.employeeToDisplay = this.employees[0];
-    this.arrayIndex = 1;
+          else  {
+          this.employeeToDisplay = this.employees[0];
+          this.arrayIndex = 1;
+          }
+  }*/
+   onClick(employeeId: number) {
+         this._router.navigate(['/employees', employeeId]); //link parameters array
    }
-  }
-
 }
